e1dd0d5fa0e53b6eb3b011c0ef67971695470115
Zhilenkov Artem <zhilenkovartem@mail.ru>
2021-10-24 12:37:20 +0300

    Create Module 5

---
+++ CSHARP/Module5/Task_5_5_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module5
+{
+    class Task_5_5_3
+    {
+        static void Main(string[] args)
+        {
+            Echo(Console.ReadLine());
+            Console.ReadKey();
+        }
+        static void Echo(string str)
+        {
+            Console.BackgroundColor = (ConsoleColor)str.Length;
+            Console.WriteLine(str);
+            if (str.Length > 1)
+            {
+                str = str.Remove(0, 1);
+                Echo(str);
+            }
+        }
+    }
+}
